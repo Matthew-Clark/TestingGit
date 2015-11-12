a0ba0eb87287ac99324ad65fd54acdf12bb3105d
matthew-clark <matthew.clark@epicgames.net>
2015-11-12 14:59:29 -0500

    Testing Git with this project - First commit

---
+++ Source/TestingGit/TestingGitGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TestingGitGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGIT_API ATestingGitGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
